e4a0d733b4f50e19562967494574b7bea37676e9
Dana Olson <dana@wadevo.com>
2014-02-21 01:13:49 -0500

    fixed bug, added launch-/update-only options

--- godot.sh
+++ godot.sh
@@ -14,6 +14,9 @@
 # 1.2 - add MacOSX build to download list
 # 1.3 - add URL for downloads, as it is now public
 # 1.4 - version.txt file no longer generated on server, script now uses timestamp on Linux binary
+# 1.5 - fixed bug where timestamp builds weren't considered newer than old version.txt builds
+#     - added option to check for updates only, do not launch Godot. call ./godot.sh update
+#     - added option to skip updates and just launch latest installed. call ./godot.sh launch
 #
 #------------------------START VARS------------------------
 
@@ -30,48 +33,55 @@ ENGINEURL=http://www.godotengine.org/builds
 #-------------------------END VARS-------------------------
 #
 
-mkdir -p $ENGINEPATH
-cd $ENGINEPATH
-
-LOCALBUILD=`find build-*/godot_x11.64 2> /dev/null | sort | tail -1 | sed 's/build-//g;s/\/godot_x11.64//g'`
-LOCALBUILD=${LOCALBUILD:-0}
-
-REMOTEDATE=`wget --server-response --spider http://www.godotengine.org/builds/release/godot_x11.64 2>&1 | grep -i last-modified | awk -F": " '{ print $2 }'`
-LATESTBUILD=`date -d "$REMOTEDATE" +%Y%m%d%H%M`
-LATESTBUILD=${LATESTBUILD:-0}
-
-echo "Local build: $LOCALBUILD, Latest release: $LATESTBUILD."
-
-if [ $LATESTBUILD -gt $LOCALBUILD ]
+# if we did not choose to only launch, let's run the update block
+if [[ $@ != launch ]]
 then
-        mkdir -p build-$LATESTBUILD
-        cd build-$LATESTBUILD
-        echo -n "Downloading new build: "
-        for i in \
-                $ENGINEURL/release/godot \
-                $ENGINEURL/release/godot_x11.64 \
-                $ENGINEURL/release/linux_server \
-                $ENGINEURL/release/linux_server.64 \
-                $ENGINEURL/release/godot_win32.exe \
-                $ENGINEURL/release/godot_win64.exe \
-                $ENGINEURL/release/GodotOSX32.zip \
-                $ENGINEURL/templates/export_templates.zip \
-                $ENGINEURL/demos/godot_demos.zip
-        do
-                wget -q -c $i
-                echo -n "*"
-        done
-        echo "*"
-        echo "Done!"
+
+	mkdir -p $ENGINEPATH
+	cd $ENGINEPATH
+
+	LOCALBUILD=`find build-*/godot_x11.64 2> /dev/null | sort -V | tail -1 | sed 's/build-//g;s/\/godot_x11.64//g'`
+	LOCALBUILD=${LOCALBUILD:-0}
+
+	REMOTEDATE=`wget --server-response --spider http://www.godotengine.org/builds/release/godot_x11.64 2>&1 | grep -i last-modified | awk -F": " '{ print $2 }'`
+	LATESTBUILD=`date -d "$REMOTEDATE" +%Y%m%d%H%M`
+	LATESTBUILD=${LATESTBUILD:-0}
+
+	echo "Local build: $LOCALBUILD, Latest release: $LATESTBUILD."
+
+	if [ $LATESTBUILD -gt $LOCALBUILD ]
+	then
+		mkdir -p build-$LATESTBUILD
+		cd build-$LATESTBUILD
+		echo -n "Downloading new build: "
+		for i in \
+			$ENGINEURL/release/godot \
+			$ENGINEURL/release/godot_x11.64 \
+			$ENGINEURL/release/linux_server \
+			$ENGINEURL/release/linux_server.64 \
+			$ENGINEURL/release/godot_win32.exe \
+			$ENGINEURL/release/godot_win64.exe \
+			$ENGINEURL/release/GodotOSX32.zip \
+			$ENGINEURL/templates/export_templates.zip \
+			$ENGINEURL/demos/godot_demos.zip
+		do
+			wget -q -c $i
+			echo -n "*"
+		done
+		echo "*"
+		echo "Done!"
+	fi
 fi
 
 
-# now launch the latest version
+# if we aren't only updating, launch the latest version
+if [[ $@ != update ]]
+then
+	mkdir -p $PROJECTPATH
+	cd $PROJECTPATH
 
-mkdir -p $PROJECTPATH
-cd $PROJECTPATH
+	CURRENTBUILD=`find $ENGINEPATH/build-*/godot_x11.64 | sort -V | tail -1`
 
-CURRENTBUILD=`find $ENGINEPATH/build-*/godot_x11.64 | sort | tail -1`
-
-chmod +x $CURRENTBUILD
-exec $CURRENTBUILD
+	chmod +x $CURRENTBUILD
+	exec $CURRENTBUILD
+fi
